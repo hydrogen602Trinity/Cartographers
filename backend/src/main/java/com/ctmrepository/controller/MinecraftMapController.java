f7dc2f5922c38fa073feeb3cdd554776480ffdab
FleckerBr <bflecker@trinity.edu>
2022-04-06 20:59:45 -0500

    Changed localhost port to 8080 from 8081

--- backend/src/main/java/com/ctmrepository/controller/MinecraftMapController.java
+++ backend/src/main/java/com/ctmrepository/controller/MinecraftMapController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class MinecraftMapController {
