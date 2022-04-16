2b493596e5772945ab5215ad210cc6c7ecd11db5
Bryton Flecker <bflecker@trinity.edu>
2022-04-15 22:12:29 -0500

    REST API Improvements (#41)
    * Implemented getMapById method, to return map with a param id. Outputs are map with Status Found, or just Status Not_Found
    
    * Sort function Basics Created, Partly UNTESTED
    
    * Changed the current sort function to output map IDs as opposed to the maps
    
    * GetMapById tested, searchThroughMaps tested, computer and project setup errors fixed, everything is going swimmingly
    
    * Pagination added to search
    
    Co-authored-by: mborenst <60239769+mborenst@users.noreply.github.com>

--- backend/src/main/java/com/ctmrepository/controller/MinecraftMapController.java
+++ backend/src/main/java/com/ctmrepository/controller/MinecraftMapController.java
@@ -1,7 +1,12 @@
 package com.ctmrepository.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 import com.ctmrepository.model.MinecraftMap;
 import com.ctmrepository.repository.MinecraftMapRepository;
@@ -9,15 +14,32 @@ import com.ctmrepository.repository.MinecraftMapRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/")
 public class MinecraftMapController {
+
+    public static <T> List<T> paginateList(List<T> list, Integer page, Integer resultsPerPage) {
+        Integer fromIndex = (page - 1) * resultsPerPage;
+        Integer toIndex = fromIndex + resultsPerPage;
+
+        if (list.size() >= toIndex) {
+            return list.subList(fromIndex, toIndex);
+        } else if (list.size() >= fromIndex) {
+            return list.subList(fromIndex, list.size());
+        } else {
+            return new ArrayList<T>();
+        }
+    }
 
     @Autowired
     MinecraftMapRepository minecraftMapRepository;
@@ -27,15 +49,26 @@ public class MinecraftMapController {
         return "Greetings from Spring Boot!";
     }
 
+    /**
+     * Sorts map database according to query and returns results corresponding to
+     * given page and response limit.
+     *
+     * @param q     search query
+     * @param limit maximum number of results to return per page
+     * @param page  page number of results to return
+     */
     @CrossOrigin(origins = { "http://localhost:3000", "https://hydrogen602trinity.github.io/" })
-    @GetMapping("/maps/search")
-    public ResponseEntity<List<MinecraftMap>> getMapSearch() {
+    @GetMapping("/search/maps")
+    public ResponseEntity<List<MinecraftMap>> getMapSearch(
+            @RequestParam() String q,
+            @RequestParam(required = false, defaultValue = "1") @Min(1) int page,
+            @RequestParam(required = false, defaultValue = "20") @Min(1) @Max(100) int per_page) {
         try {
             List<MinecraftMap> maps = new ArrayList<MinecraftMap>();
 
-            minecraftMapRepository.findAll().forEach(maps::add);
-
-            System.out.println("it runs");
+            q = q.replaceAll("_", " ");
+            sortByRelevance(minecraftMapRepository.findAll(), q.toUpperCase()).forEach(maps::add);
+            maps = paginateList(maps, page, per_page);
 
             return new ResponseEntity<>(maps, HttpStatus.OK);
         } catch (Exception e) {
@@ -44,37 +77,105 @@ public class MinecraftMapController {
         }
     }
 
-    // @GetMapping("/maps")
-    // public ResponseEntity<List<MinecraftMap>> getAllMaps(@RequestParam(required =
-    // false) String name) {
-    // try {
-    // List<MinecraftMap> maps = new ArrayList<MinecraftMap>();
+    @GetMapping("/maps/{id}")
+    public ResponseEntity<MinecraftMap> getMapById(@PathVariable("id") long id) {
+        Optional<MinecraftMap> mapData = minecraftMapRepository.findById(id);
 
-    // if (name == null)
-    // minecraftMapRepository.findAll().forEach(maps::add);
-    // else
-    // minecraftMapRepository.findByNameContaining(name).forEach(maps::add);
+        if (mapData.isPresent()) {
+            return new ResponseEntity<>(mapData.get(), HttpStatus.FOUND);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
-    // if (maps.isEmpty()) {
-    // return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    // }
+    // Next, given a list of maps and the search string,
+    // sort the list of maps by the Levenshtein Distances and Return
+    public List<MinecraftMap> sortByRelevance(List<MinecraftMap> maps, String search) {
+        // Get Levenshtein Distances for names
+        List<Integer> levenschteinValues = new ArrayList<Integer>();
+        for (int i = 0; i < maps.size(); i++) {
+            levenschteinValues
+                    .add(Integer.valueOf(getLevenshteinDistance(maps.get(i).getName().toUpperCase(), search)));
+        }
 
-    // return new ResponseEntity<>(maps, HttpStatus.OK);
-    // } catch (Exception e) {
-    // return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-    // }
-    // }
+        // Custom Sort by Levenshtein Distances
+        // Get smallest relative distance, throw it into the new map, repeat, n^2 time
+        List<MinecraftMap> sortedMaps = new ArrayList<MinecraftMap>();
+        while (!maps.isEmpty()) {
+            int index = -1;
+            Integer indexValue = Integer.MAX_VALUE;
+            for (int i = 0; i < levenschteinValues.size(); i++) {
+                if (levenschteinValues.get(i) < indexValue) {
+                    index = i;
+                    indexValue = levenschteinValues.get(index);
+                }
+            }
+            sortedMaps.add(maps.get(index));
+            maps.remove(index);
+            levenschteinValues.remove(index);
+        }
+        return sortedMaps;
+    }
 
-    // @GetMapping("/maps/{id}")
-    // public ResponseEntity<MinecraftMap> getMapById(@PathVariable("id") long id) {
-    // Optional<MinecraftMap> mapData = minecraftMapRepository.findById(id);
+    // A comparison where the larger the int the more different the strings are
+    // Made by the number of addition, subtractions, or substitutions needed to
+    // match x to y
+    public int getLevenshteinDistance(String x, String y) {
+        int[][] dp = new int[x.length() + 1][y.length() + 1];
 
-    // if (mapData.isPresent()) {
-    // return new ResponseEntity<>(mapData.get(), HttpStatus.OK);
-    // } else {
-    // return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    // }
-    // }
+        for (int i = 0; i <= x.length(); i++) {
+            for (int j = 0; j <= y.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else {
+                    dp[i][j] = min(dp[i - 1][j - 1]
+                            + costOfSubstitution(x.charAt(i - 1), y.charAt(j - 1)),
+                            dp[i - 1][j] + 1,
+                            dp[i][j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[x.length()][y.length()];
+    }
+
+    // return if there is a substitution cost or not
+    public static int costOfSubstitution(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+
+    // return the smallest of the int numbers
+    public static int min(int... numbers) {
+        return Arrays.stream(numbers)
+                .min().orElse(Integer.MAX_VALUE);
+    }
+
+    /*
+     * @GetMapping("/maps")
+     * public ResponseEntity<List<MinecraftMap>> getAllMaps(@RequestParam(required =
+     * false) String name) {
+     * try {
+     * List<MinecraftMap> maps = new ArrayList<MinecraftMap>();
+     * 
+     * if (name == null)
+     * minecraftMapRepository.findAll().forEach(maps::add);
+     * else
+     * // Search for Name of Map
+     * minecraftMapRepository.findAll().forEach(maps::add);
+     * // minecraftMapRepository.findByNameContaining(name).forEach(maps::add);
+     * 
+     * if (maps.isEmpty()) {
+     * return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+     * }
+     * 
+     * return new ResponseEntity<>(maps, HttpStatus.OK);
+     * } catch (Exception e) {
+     * return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+     * }
+     * }
+     */
 
     // @PostMapping("/maps")
     // public ResponseEntity<MinecraftMap> createMap(@RequestBody MinecraftMap map)
