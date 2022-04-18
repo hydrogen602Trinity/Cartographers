4ee1fa47c4e70910201d6b7a5710435b8970d496
hydrogen602 <hydrogen31415@gmail.com>
2022-04-18 17:41:03 -0500

    CORS issues fix attempt

--- backend/src/main/java/com/ctmrepository/controller/MinecraftMapController.java
+++ backend/src/main/java/com/ctmrepository/controller/MinecraftMapController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
-@CrossOrigin(origins = { "http://localhost:3000/", "https://hydrogen602trinity.github.io/" })
+@CrossOrigin(origins = { "http://localhost:3000", "https://hydrogen602trinity.github.io" })
 @RestController
 @RequestMapping("/")
 public class MinecraftMapController {
