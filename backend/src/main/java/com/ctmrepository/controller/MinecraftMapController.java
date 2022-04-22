d1b24e64379803f53afc2c1535156d728db3bff4
mborenst <60239769+mborenst@users.noreply.github.com>
2022-04-22 13:22:57 -0500

    I say a solemn farewell to the levenshtein distance

--- backend/src/main/java/com/ctmrepository/controller/MinecraftMapController.java
+++ backend/src/main/java/com/ctmrepository/controller/MinecraftMapController.java
@@ -265,40 +265,6 @@ public class MinecraftMapController {
                 / 3.0;
     }    
     
-    // A comparison where the larger the int the more different the strings are
-    // Made by the number of addition, subtractions, or substitutions needed to
-    // match x to y
-    public int getLevenshteinDistance(String x, String y) {
-        int[][] dp = new int[x.length() + 1][y.length() + 1];
-
-        for (int i = 0; i <= x.length(); i++) {
-            for (int j = 0; j <= y.length(); j++) {
-                if (i == 0) {
-                    dp[i][j] = j;
-                } else if (j == 0) {
-                    dp[i][j] = i;
-                } else {
-                    dp[i][j] = min(dp[i - 1][j - 1]
-                            + costOfSubstitution(x.charAt(i - 1), y.charAt(j - 1)),
-                            dp[i - 1][j] + 1,
-                            dp[i][j - 1] + 1);
-                }
-            }
-        }
-        return dp[x.length()][y.length()];
-    }
-
-    // return if there is a substitution cost or not
-    public static int costOfSubstitution(char a, char b) {
-        return a == b ? 0 : 3;
-    }
-
-    // return the smallest of the int numbers
-    public static int min(int... numbers) {
-        return Arrays.stream(numbers)
-                .min().orElse(Integer.MAX_VALUE);
-    }
-
     /*
      * @GetMapping("/maps")
      * public ResponseEntity<List<MinecraftMap>> getAllMaps(@RequestParam(required =
