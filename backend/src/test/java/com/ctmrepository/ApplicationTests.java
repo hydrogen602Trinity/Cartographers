6b92e53fcf596207f04b64d99ec726e0cb3277b8
mborenst <60239769+mborenst@users.noreply.github.com>
2022-04-28 01:36:19 -0500

    Some Test Code Testing, now correct and robust

--- backend/src/test/java/com/ctmrepository/ApplicationTests.java
+++ backend/src/test/java/com/ctmrepository/ApplicationTests.java
@@ -1,12 +1,16 @@
 package com.ctmrepository;
 
 import com.ctmrepository.controller.MinecraftMapController;
+import com.ctmrepository.model.MinecraftMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
 @SpringBootTest
 class ApplicationTests {
@@ -30,60 +34,68 @@ class ApplicationTests {
     void mapsAreAccessible() throws Exception {
         assertThat(controller.getMapCount().getStatusCodeValue() > 0).isTrue();
         int size = controller.getMapCount().getStatusCodeValue();
-        assertThat(controller.getMapById(0)).isNull();
+        assertThat(controller.getMapById(0).getStatusCode().equals(HttpStatus.NOT_FOUND)).isTrue();
         for (int i = 1; i <= size; i++) {
             assertThat(controller.getMapById(i)).isNotNull();
         }
     }
 
     @Test
-    void mapsAreSearchable() throws Exception {
+    void mapsAreStrictSearchable() throws Exception {
+        // Test that Maps are searchable
+        assertThat(controller.getMapSearch("", 1, 20, true)).isNotNull();
+
+        // General Case
+        int max_test_map_size = Math.min(controller.getMapCount().getBody() / 2, 25);
+        MinecraftMap[] testMaps = new MinecraftMap[max_test_map_size];
+        for (int i = 0; i < testMaps.length; i++) {
+            testMaps[i] = controller.getMapById(
+                    (int) (Math.random() * (controller.getMapCount().getBody())))
+                    .getBody();
+        }
+        for (MinecraftMap map : testMaps) {
+            List<MinecraftMap> searchMap = controller
+                    .getMapSearch(map.getName(), 1, 20, true).getBody();
+            assertThat(searchMap.stream().filter(o -> o.getId() == map.getId())
+                    .findFirst().isPresent()).isTrue();
+        }
+
+        // Specific Case
+        List<MinecraftMap> searchMap = controller
+                .getMapSearch("Tecnocraft2802", 1, 20, true).getBody();
+        assertThat(searchMap.stream().filter(o -> o.getId() == 6)
+                .findFirst().isPresent()).isTrue();
+        assertThat(searchMap.stream().filter(o -> o.getId() == 7)
+                .findFirst().isPresent()).isTrue();
+    }
+
+    @Test
+    void mapsAreFuzzySearchable() throws Exception {
         // Test that Maps are searchable
         assertThat(controller.getMapSearch("", 1, 20, true)).isNotNull();
         assertThat(controller.getMapSearch("", 1, 20, false)).isNotNull();
 
-        // Get maps through Hard Search
-        assertThat(controller.getMapSearch("Moon", 1, 20, true)
-                .getBody().contains(controller
-                        .getMapById(1).getBody()))
-                .isTrue();
-        assertThat(controller.getMapSearch("Monstrosity", 1, 20, true)
-                .getBody().contains(controller
-                        .getMapById(3).getBody()))
-                .isTrue();
-        assertThat(controller.getMapSearch("Tala", 1, 20, true)
-                .getBody().contains(controller
-                        .getMapById(4).getBody()))
-                .isTrue();
-        assertThat(controller.getMapSearch("Tecnocraft2802", 1, 20, true)
-                .getBody().contains(controller
-                        .getMapById(6).getBody()))
-                .isTrue();
-        assertThat(controller.getMapSearch("Tecnocraft2802", 1, 20, true)
-                .getBody().contains(controller
-                        .getMapById(7).getBody()))
-                .isTrue();
+        // General Case
+        int max_test_map_size = Math.min(controller.getMapCount().getBody() / 2, 25);
+        MinecraftMap[] testMaps = new MinecraftMap[max_test_map_size];
+        for (int i = 0; i < testMaps.length; i++) {
+            testMaps[i] = controller.getMapById(
+                    (int) (Math.random() * (controller.getMapCount().getBody())))
+                    .getBody();
+        }
+        for (MinecraftMap map : testMaps) {
+            List<MinecraftMap> searchMap = controller
+                    .getMapSearch(map.getName(), 1, 20, false).getBody();
+            assertThat(searchMap.stream().filter(o -> o.getId() == map.getId())
+                    .findFirst().isPresent()).isTrue();
+        }
 
-        // Get Maps Through Soft Search
-        assertThat(controller.getMapSearch("Moon", 1, 20, false)
-                .getBody().contains(controller
-                        .getMapById(1).getBody()))
-                .isTrue();
-        assertThat(controller.getMapSearch("Monstrosity", 1, 20, false)
-                .getBody().contains(controller
-                        .getMapById(3).getBody()))
-                .isTrue();
-        assertThat(controller.getMapSearch("Tala", 1, 20, false)
-                .getBody().contains(controller
-                        .getMapById(4).getBody()))
-                .isTrue();
-        assertThat(controller.getMapSearch("Tecnocraft2802", 1, 20, false)
-                .getBody().contains(controller
-                        .getMapById(6).getBody()))
-                .isTrue();
-        assertThat(controller.getMapSearch("Tecnocraft2802", 1, 20, false)
-                .getBody().contains(controller
-                        .getMapById(7).getBody()))
-                .isTrue();
+        // Specific Case
+        List<MinecraftMap> searchMap = controller
+                .getMapSearch("Tecnocraft2802", 1, 20, false).getBody();
+        assertThat(searchMap.stream().filter(o -> o.getId() == 6)
+                .findFirst().isPresent()).isTrue();
+        assertThat(searchMap.stream().filter(o -> o.getId() == 7)
+                .findFirst().isPresent()).isTrue();
     }
 }
