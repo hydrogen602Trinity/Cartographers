80f7a3b520eb815d2b16f79997e0228a6ee80776
FleckerBr <bflecker@trinity.edu>
2022-04-29 13:47:19 -0500

    Added typedoc generation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,27 @@
 import { SnackbarComponent } from 'components/Snackbar';
-import React from 'react';
-import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from 'pages/Home';
 import MapView from 'pages/MapView';
 import NoMatch from 'pages/NoMatch';
+import { HashRouter, Route, Routes } from "react-router-dom";
 
+/**
+ * HashRouter basename uses '/' instead of 'process.env.PUBLIC_URL'
+ * because GitHub pages will append basename to repository name if included.
+ * If HashRouter is replaced with BrowserRouter, basename should be changed
+ * to the following: `basename={process.env.PUBLIC_URL}`
+ * @returns {JSX.Element}
+ */
 function App() {
   return (
-    <React.StrictMode>
-      <HashRouter basename="/">
-        <SnackbarComponent>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/maps/:id/*" element={<MapView />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </SnackbarComponent>
-      </HashRouter>
-    </React.StrictMode>
+    <HashRouter basename="/">
+      <SnackbarComponent>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/maps/:id/*" element={<MapView />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </SnackbarComponent>
+    </HashRouter>
   );
 }
 
